08699fea9119ae5ce66a7bbffb3521f8d3f14525
Jonathan Queiroz <dev@johnjq.com>
2017-09-18 12:45:39 -0300

    Change version to 0.2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='sslib',
-    version='0.1.0',
+    version='0.2.0',
     author='Jonathan Queiroz',
     author_email='dev@johnjq.com',
     url='https://github.com/jqueiroz/python-sslib',
